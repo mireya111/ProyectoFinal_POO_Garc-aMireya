3bf616c8ea4e711b6e61c44297b67268692675c9
mireya111 <mireya.garcia@epn.edu.ec>
2024-07-21 21:49:24 -0500

    version1_Mireya Garcia

---
+++ src/Login.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login {
+    private JPanel panelLogin;
+    private JTextField correo;
+    private JTextField contrasenia;
+    private JButton entrarButton;
+    private JButton registroClienteButton;
+    private JButton registroEmpleadosButton;
+    private JLabel errores;
+
+    public Login() {
+        entrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
